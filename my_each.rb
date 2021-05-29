c1e9a9e856f94d2f116c3d874082bf54bb983d9b
Ian King <kickthechair@gmail.com>
2021-05-29 15:28:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+array = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
+  i = 0
+  
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
 end
+
+
+p my_each(array)
